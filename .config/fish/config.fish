a686abb1bec8e4df711dfb92f930da654f343044
Jorge L. Fatta <jorge.fatta@auth0.com>
2022-04-22 12:48:32 -0300

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -14,10 +14,27 @@ alias vim=nvim
 alias v=nvim
 alias k=kubectl
 
+export PATH="/usr/local/opt/libpq/bin:$PATH"
 export KUB_EDITOR="nvim"
+export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/3.0.0/bin:$PATH"
 export PATH="$HOME/.gem/ruby/3.0.0/bin:$PATH"
 export PATH="$HOME/.gem/ruby/3.0.1/bin:$PATH"
 
+
+# krew K8s plugin manager
+set -gx PATH $PATH $HOME/.krew/bin
+
+function dark
+  theme_gruvbox dark hard
+end
+
+function light
+  theme_gruvbox light hard
+end
+
+source private.fish
+
+change_background
 
 direnv hook fish | source
 
